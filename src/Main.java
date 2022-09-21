cf5fa237e0aff4577a3f82fae0409f09634c8441
Zia999 <92579255+Zia999@users.noreply.github.com>
2022-09-21 15:53:17 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value =" + myMinIntValue);
+        System.out.println("Integer Minimum Value =" + myMaxIntValue);
+
+    }
+}
